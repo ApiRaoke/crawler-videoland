def3ae2877c6210d1eb521c91cfcd75dabf5b3fc
Rogerio Prado de Jesus <rogeriopradoj@gmail.com>
2018-01-30 10:14:35 -0200

    Add more fiels in result file (hash md5, total_pages, current_page, music_page_order and current_artist_search)

--- crawler.py
+++ crawler.py
@@ -1,5 +1,6 @@
 import string
 import scrapy
+import hashlib
 
 class VideolandCrawler(scrapy.Spider):
 
@@ -18,14 +19,51 @@ class VideolandCrawler(scrapy.Spider):
             PAGINA_SELECTOR).extract_first().split()[-1])
         # print(total_pages)
 
+        yield scrapy.Request(
+            response.url,
+            callback=self.parse_page,
+            meta={'total_pages': total_pages}
+        )
+
+        for contador_paginas in range(1, total_pages + 1):
+            url_current_page = response.url
+            current_page_number = str(url_current_page.split('=')[-1])
+
+            url_next_page = ''.join(
+                url_current_page.rsplit(
+                    current_page_number, 1)
+                ) + str(contador_paginas)
+
+            yield scrapy.Request(
+                response.urljoin(url_next_page),
+                callback=self.parse
+            )
+
+    def parse_page(self, response):
         for contador_musicas in range(2, 42):
 
             CANTOR_SELECTOR = '//tr[{0:d}]/td[1]/font/text()'.format(contador_musicas)
             CODIGO_SELECTOR = '//tr[{0:d}]/td[2]/font/text()'.format(contador_musicas)
             TITULO_SELECTOR = '//tr[{0:d}]/td[3]/font/text()'.format(contador_musicas)
-            LETRA_SELECTOR  = '//tr[{0:d}]/td[4]/font/text()'.format(contador_musicas)
+            LETRA_SELECTOR = '//tr[{0:d}]/td[4]/font/text()'.format(contador_musicas)
             IDIOMA_SELECTOR = '//tr[{0:d}]/td[5]/font/text()'.format(contador_musicas)
             PACOTE_SELECTOR = '//tr[{0:d}]/td[6]/font/a/text()'.format(contador_musicas)
+
+            url = response.url
+
+            total_pages = response.meta.get('total_pages')
+            current_page = url.split('=')[-1]
+            
+            current_artist_search = url.split('=')[-3].split('&')[0]
+
+            music_page_order = contador_musicas - 1
+
+            cantor = response.xpath(CANTOR_SELECTOR).extract_first()
+            codigo = response.xpath(CODIGO_SELECTOR).extract_first()
+            titulo = response.xpath(TITULO_SELECTOR).extract_first()
+            letra =  response.xpath(LETRA_SELECTOR).extract_first()
+            idioma = response.xpath(IDIOMA_SELECTOR).extract_first()
+            pacote = response.xpath(PACOTE_SELECTOR).extract_first()
 
             # cantor: CANTOR
             # codigo: CÓD
@@ -35,46 +73,21 @@ class VideolandCrawler(scrapy.Spider):
             # pacote: PACOTE
 
             yield {
+                'md5': hashlib.md5(
+                    (
+                        url + str(total_pages) + cantor + codigo +
+                        titulo + letra + idioma + pacote
+                    ).encode('utf-8')
+                ).hexdigest(),
+                'url': url,
                 'total_pages': total_pages,
-                'url':    response.url,
-                'cantor': ''.join(response.xpath(CANTOR_SELECTOR).extract()),
-                'codigo': ''.join(response.xpath(CODIGO_SELECTOR).extract()),
-                'titulo': ''.join(response.xpath(TITULO_SELECTOR).extract()),
-                'letra':  ''.join(response.xpath(LETRA_SELECTOR).extract()),
-                'idioma': ''.join(response.xpath(IDIOMA_SELECTOR).extract()),
-                'pacote': ''.join(response.xpath(PACOTE_SELECTOR).extract()),
+                'current_page': current_page,
+                'music_page_order': music_page_order,
+                'current_artist_search': current_artist_search,
+                'cantor': cantor,
+                'codigo': codigo,
+                'titulo': titulo,
+                'letra': letra,
+                'idioma': idioma,
+                'pacote': pacote,
             }
-
-        total_mais_1 = total_pages + 1
-
-        for contador_paginas in range(1, total_mais_1):
-            url_current_page = response.url
-            current_page_number = str(url_current_page.split('=')[-1])
-
-            url_next_page = ''.join(url_current_page.rsplit(current_page_number,1)) + str(contador_paginas)
-
-            # print(url_current_page, current_page_number, url_next_page)
-
-            yield scrapy.Request(
-                response.urljoin(url_next_page),
-                callback=self.parse
-            )
-
-
-
-# em cada url (passando por todas as páginas)
-
-    ## em cada página 
-    
-# trazer para o banco:
-
-# cantor: CANTOR
-# codigo: CÓD
-# titulo: TÍTULO
-# letra:  INÍCIO DA LETRA
-# idioma: IDIOMA
-# pacote: PACOTE
-
-
-# Exemplo:
-# https://www.videoland.com.br/wwwroot/pesquisa.asp?artista=A&categoria=33&pagina=1
